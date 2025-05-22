40929bc5bb1bf73f5e7e8a8f8edceee14f22e448
urakawa_nanami <urakawa.nanami@alhinc.jp>
2025-05-22 16:53:55 +0900

    応用編(簡易Twitter)作成途中

---
+++ src/chapter6/service/MessageService.java
@@ -0,0 +1,55 @@
+package chapter6.service;
+
+import static chapter6.utils.CloseableUtil.*;
+import static chapter6.utils.DBUtil.*;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import chapter6.beans.Message;
+import chapter6.dao.MessageDao;
+
+public class MessageService {
+
+
+    /**
+    * ロガーインスタンスの生成
+    */
+    Logger log = Logger.getLogger("twitter");
+
+    /**
+    * デフォルトコンストラクタ
+    * アプリケーションの初期化を実施する。
+    */
+    public MessageService() {
+    	 public List<UserMessage> select() {
+
+    		  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
+    	        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+
+    	        final int LIMIT_NUM = 1000;
+
+    	        Connection connection = null;
+    	        try {
+    	            connection = getConnection();
+    	            List<UserMessage> messages = new UserMessageDao().select(connection, LIMIT_NUM);
+    	            commit(connection);
+
+    	            return messages;
+    	        } catch (RuntimeException e) {
+    	            rollback(connection);
+    			log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+    	            throw e;
+    	        } catch (Error e) {
+    	            rollback(connection);
+    			log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);
+    	            throw e;
+    	        } finally {
+    	            close(connection);
+    	        }
+    	    }
+    	}
+
+}
